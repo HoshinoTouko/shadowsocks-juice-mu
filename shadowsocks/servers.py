3860d2f8d5109f0fe4f1a4bd9907ba29569d446d
oranly@hotmail.com <oranly@hotmail.com>
2017-07-25 17:55:03 +0800

    Add copyright info

--- shadowsocks/servers.py
+++ shadowsocks/servers.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright 2015 clowwindy
+# Copyright 2017 HoshinoTouko
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
